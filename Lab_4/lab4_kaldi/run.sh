c9da2bde2d42238a1cbce3bcdeee8ffff800feb1
Marco Ravelo <mravelo5874@gmail.com>
2022-10-18 12:27:00 -0500

    Update run.sh

--- Lab_4/lab4_kaldi/run.sh
+++ Lab_4/lab4_kaldi/run.sh
@@ -13,12 +13,12 @@ stage=3
 set -euo pipefail
 
 if [ $stage -le 0 ]; then
-  echo "stage 0"
+  echo "[stage 0]"
   local/download_lm.sh $lm_url $data data/local/lm
 fi
 
 if [ $stage -le 1 ]; then
-  echo "stage 1"
+  echo "[stage 1]"
   # format the data as Kaldi data directories
   for part in dev-clean-2 train-clean-5; do
     # use underscore-separated names in data directories.
@@ -35,7 +35,7 @@ if [ $stage -le 1 ]; then
 fi
 
 if [ $stage -le 2 ]; then
-  echo "stage 2"
+  echo "[stage 2]"
   mfccdir=mfcc
   # extract MFCCs for train and test data
   steps/make_mfcc.sh data/train_clean_5 exp/make_mfcc $mfccdir
@@ -45,7 +45,7 @@ fi
 
 # train a monophone system
 if [ $stage -le 3 ]; then
-  echo "stage 3"
+  echo "[stage 3]"
   utils/subset_data_dir.sh --shortest data/train_clean_5 500 data/train_500short
   # train a monophone acoustic model
   steps/train_mono.sh --boost-silence 1.25 data/train_500short data/lang_nosp exp/mono
@@ -53,13 +53,15 @@ fi
 
 # train a delta + delta-delta triphone system on all utterances
 if [ $stage -le 4 ]; then
-  echo "stage 4"
-  # TODO: 1) force-align the entire training set with the monophone model
-  # TODO: 2) train the triphone model on the entire training set
+  echo "[stage 4]"
+  # 1) force-align the entire training set with the monophone model
+  steps/align_si.sh --boost-silence 1.25 data/train_clean_5 data/lang_nosp exp/mono exp/mono_ali_train_clean_5
+  # 2) train the triphone model on the entire training set
+  steps/train_deltas.sh 2000 100000 data/train_clean_5 data/lang_nosp exp/mono_ali_train_clean_5 exp/tri1
 fi
 
 if [ $stage -le 5 ]; then
-  echo "stage 5"
+  echo "[stage 5]"
   # TODO: 1) build the decoding graph
   # TODO: 2) decode using the triphone model
   # TODO: 3) rescore with the larger (tgmed) language model
